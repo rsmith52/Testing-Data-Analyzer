632466dd9e2ef573f722dcf52e2ed137018df287
ray <rsmith52@wisc.edu>
2019-03-25 18:40:06 -0500

    Created cell class to for individual cells in network

--- Neural Network/cell.java
+++ Neural Network/cell.java
@@ -1,4 +1,7 @@
 public class cell {
 
-  String functionType; // ReLU
+  String functionType; // ReLU or Sigmoid
+  final int bias = 1; // Bias term added to function each time
+  double[] input weights; // Weights for all inputs including bias term as index 0
+  
 }
--- Neural Network/neural.java
+++ Neural Network/neural.java
@@ -1,11 +1,14 @@
 public class neural {
 
   // Holds weights (double)
-  // Holds cells and connections (need new class/type) - weights, inputs, outputs, function
+  Cell[] firstLayer;
+  Cell[] secondLayer;
   // Holds functions (math functions for RELU and Sigmoid)
   // Holds inputs (list of input variables)
   // Holds outputs/labels (list of output labels)
 
 
   // getters and setters for all the above
+
+
 }
