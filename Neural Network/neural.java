011f71e12d36be4a6b5109b1c9c3b2c34f38dd5d
ray <rsmith52@wisc.edu>
2019-03-25 18:09:01 -0500

    Added some notes in run_neural and neural

---
+++ Neural Network/neural.java
@@ -0,0 +1,11 @@
+public class neural {
+
+  // Holds weights (double)
+  // Holds cells and connections (need new class/type)
+  // Holds functions (math functions for RELU and Sigmoid)
+  // Holds inputs (list of input variables)
+  // Holds outputs/labels (list of output labels)
+
+
+  // getters and setters for all the above
+}
---
+++ Neural Network/run_neural.java
@@ -0,0 +1,8 @@
+public class run_neural.java {
+
+  // Method runs a single case through the neural network and returns an array of outputs
+  public float[] runNetwork(Neural network, Case case) {
+
+  }
+
+}
