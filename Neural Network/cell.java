5a433c0bc045d57ed505c90cca4f1f26405039de
ray <rsmith52@wisc.edu>
2019-03-25 18:40:50 -0500

    Added clarification in cell.java

--- Neural Network/cell.java
+++ Neural Network/cell.java
@@ -3,5 +3,12 @@ public class cell {
   String functionType; // ReLU or Sigmoid
   final int bias = 1; // Bias term added to function each time
   double[] input weights; // Weights for all inputs including bias term as index 0
-  
+
+  // function - runs RELU or Sigmoid based on functionType field
+
+  // RELU function
+
+
+  // Sigmoid function
+
 }
