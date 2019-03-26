d77bf6c530626d463700beb18140b3fb63988dda
ray <rsmith52@wisc.edu>
2019-03-25 19:10:34 -0500

    Added first functions in train_neural.java to overview the process and find the error for the top parts of the network so far

--- Neural Network/cell.java
+++ Neural Network/cell.java
@@ -2,7 +2,7 @@ public class cell {
 
   String functionType; // ReLU or Sigmoid
   final int bias = 1; // Bias term added to function each time
-  double[] input weights; // Weights for all inputs including bias term as index 0
+  double[] inputWeights; // Weights for all inputs including bias term as index 0
 
   // function - runs RELU or Sigmoid based on functionType field
 
