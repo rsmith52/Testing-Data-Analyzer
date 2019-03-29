2f92c2ffaa7e1ea778661415f89a5f7cbc3fd80f
ray <rsmith52@wisc.edu>
2019-03-29 14:48:38 -0500

    Got categorized.java from nate

--- src/main/java/Objects/Categorized.java
+++ src/main/java/Objects/Categorized.java
@@ -1,1 +1,28 @@
 package Objects;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Categorized implements Serializable{
+  String name;
+  int dateCreated;
+  ArrayList<Case> caseList;
+
+  public Categorized(String name, int dateCreated){
+    caseList = new ArrayList<Case>();
+    this.name = name;
+    this.dateCreated = dateCreated;
+  }
+  
+  public void addToList(Case newCase){
+    caseList.add(newCase);
+  }
+  
+  public void combineLists(ArrayList<Case> firstList,
+    ArrayList<Case> secondList){
+    ArrayList<Case> combined = new ArrayList<Case>();
+    combined.addAll(firstList);
+    combined.addAll(secondList);
+    caseList = combined;
+  }
+}
