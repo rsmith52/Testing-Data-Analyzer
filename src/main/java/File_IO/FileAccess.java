120449d9f48625091f71d4a60ce2631162a95c36
ray <rsmith52@wisc.edu>
2019-04-12 15:04:19 -0500

    New file access methods, cannot write to resource folder - is NOT POSSIBLE

--- src/main/java/File_IO/FileAccess.java
+++ src/main/java/File_IO/FileAccess.java
@@ -1,6 +1,7 @@
 package File_IO;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileAccess {
 	
@@ -15,5 +16,35 @@ public class FileAccess {
 		return null;
 		
 	}
+	
+	/*
+	 * Error List:
+	 * 	pathname is a directory : throw an IOException
+	 * 	pathname is null : throw an IOException
+	 * 	pathname is already a file : return existing file
+	 * 
+	 */
+	public static File createFile(String pathname) throws IOException
+	{
+		File file;
+		
+		//if null is passed in
+		if(pathname == null)
+		{
+			throw new IOException("Null Path");
+		}
+		try {
+			file = new File(pathname);
+			if (!file.isFile())
+			{
+				throw new IOException("Directory");
+			}
+		} catch(Exception e) {
+			throw e;
+		}
+		
+		return file;
+		
+}
 
 }
