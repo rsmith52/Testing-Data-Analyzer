df2c64ec789a41c2bed4cca85ce4f527036e3444
ray <rsmith52@wisc.edu>
2019-04-11 19:59:27 -0500

    updated testing in main2

--- src/main/java/Main2.java
+++ src/main/java/Main2.java
@@ -37,8 +37,8 @@ public class Main2 {
 	    
 	    
 	    // Train network with cases, 100 epochs
-	    System.out.println("Training network with training set 1000 times");
-	    Train_Neural.trainNeuralEpochs(network, testArray, 1000);
+	    System.out.println("Training network with training set 2000 times");
+	    Train_Neural.trainNeuralEpochs(network, testArray, 2000);
 	    System.out.println("Trained");
 	    
 	    // Run through trained network
@@ -48,12 +48,23 @@ public class Main2 {
 	    }
 	    
 	    // Print Results
+	    /*
 	    for (int i = 0; i < finalResults.get(0).length; i++) {
 	    	System.out.print(finalResults.get(0)[i] + " "); // 40% confident, but 0 on all else
 	    }
 	    System.out.println();
 	    for (int i = 0; i < finalResults.get(284).length; i++) {
 	    	System.out.print(finalResults.get(284)[i] + " "); // 99% confident
+	    }
+	    */
+	    
+	    System.out.println();
+	    System.out.println("Results of all Cases");
+	    for (int i = 0; i < finalResults.size(); i++) {
+	    	for (int j = 0; j < finalResults.get(i).length; j++) {
+	    		System.out.print(finalResults.get(i)[j] + " ");
+	    	}
+	    	System.out.println();
 	    }
 	    
 	    
