1c8cd26034164f9812e70c31dcff5f581a477294
Francesca Tyler <ftyler@wisc.edu>
2019-04-12 22:50:46 -0500

    Formatted font in table

--- src/main/java/File_IO/PDF_Out.java
+++ src/main/java/File_IO/PDF_Out.java
@@ -3,19 +3,23 @@ package File_IO;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
-//import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ArrayList;
 
 import com.itextpdf.text.Chunk;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+//import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
+//import com.itextpdf.layout.element;
+import com.itextpdf.text.FontFactory;
 
 import Objects.Case;
-//import Objects.Categorized;
-import Objects.CategorizedTest;
+import Objects.Categorized;
 
 public class PDF_Out {
 	
@@ -27,9 +31,8 @@ public class PDF_Out {
 
 	
 	public void outputPDF() {	// ArrayList<Case>() caseList, String name, String dateCreated
-		CategorizedTest categorizedTest = new CategorizedTest();
-		ArrayList<Case> cases = categorizedTest.getCaseList();
-		System.out.println(cases.size());
+		Categorized categorized = new Categorized();
+		ArrayList<Case> cases = categorized.getCaseList();
 		
 		/* Info that Case contains: 
 		 * int caseNumber, 
@@ -41,6 +44,18 @@ public class PDF_Out {
 		 * String category
 		 */
 		
+		float fntSize = 8f;
+        PdfPTable table = new PdfPTable(7);
+        Font tableFont = FontFactory.getFont(FontFactory.COURIER, fntSize);
+        table.addCell(new Phrase("Case Number", tableFont));
+        table.addCell(new Phrase("Owner", tableFont));
+        table.addCell(new Phrase("Requestor", tableFont));
+        table.addCell(new Phrase("Date Requested", tableFont));
+        table.addCell(new Phrase("Description", tableFont));
+        table.addCell(new Phrase("Tokenized Description", tableFont));
+        table.addCell(new Phrase("Category", tableFont));
+        
+		
 		try {
 			OutputStream file = new FileOutputStream(new File("PDFTest.pdf"));
 
@@ -51,7 +66,7 @@ public class PDF_Out {
 			String dateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date());
 			document.add(new Paragraph(dateTime));
 			document.add(Chunk.NEWLINE);
-			document.add(new Paragraph("Hello World, iText"));
+			document.add(table);
 
 			document.close();
 			file.close();
